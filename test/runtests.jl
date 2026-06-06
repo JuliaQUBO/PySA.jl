435867c6da17fa7013d805061d1f980cae1f6d63
David E. Bernal Neira <dbernaln@purdue.edu>
2026-06-06 08:23:09 -0400

    fix: pin PySA dependency to v0.1.0 (#18)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,16 @@ using Test
 
 @testset "Package metadata" begin
     project = TOML.parsefile(joinpath(pkgdir(PySA), "Project.toml"))
+    condapkg = TOML.parsefile(joinpath(pkgdir(PySA), "CondaPkg.toml"))
     qubodrivers_compat = split(project["compat"]["QUBODrivers"], r",\s*")
     readme = read(joinpath(pkgdir(PySA), "README.md"), String)
+    source = read(joinpath(pkgdir(PySA), "src", "PySA.jl"), String)
 
     @test "0.5" in qubodrivers_compat
     @test occursin("https://github.com/JuliaQUBO/QUBODrivers.jl", readme)
     @test !occursin("https://github.com/psrenergy/QUBODrivers.jl", readme)
+    @test condapkg["pip"]["deps"]["pysa"] == "@ git+https://github.com/nasa/pysa@v0.1.0"
+    @test occursin(r"version\s*=\s*v\"0\.1\.0\"", source)
 end
 
 QUBODrivers.test(PySA.Optimizer; examples=true) do model
